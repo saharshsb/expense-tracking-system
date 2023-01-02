9578518af798e31ab79d3ade4a5522c997040e88
saharshsb <saharshsbabu@gmail.com>
2023-01-02 09:17:37 +0530

    added

---
+++ expense_tracking_system/src/main/java/com/mycompany/expense_tracking_system/Expense_tracking_system.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.expense_tracking_system;
+
+import com.mycompany.expense_tracking_system.newpackage.Login_page;
+
+/**
+ *
+ * @author sahar
+ */
+public class Expense_tracking_system {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Login_page().setVisible(true);
+            }
+        });
+    }
+}
